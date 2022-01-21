d751cd1fcd181bff13c1a7f764bfee844bc2051d
Stefke Velja <stef.fac150@gmail.com>
2022-01-21 21:40:04 +0100

    Sve forme su zavrsene

--- src/Pages/Titles/Edit title/EditTitle.jsx
+++ src/Pages/Titles/Edit title/EditTitle.jsx
@@ -25,6 +25,7 @@ function EditTitle(props) {
   const handleImage = (e) => {
     e.persist();
     setImage({ image: e.target.files[0] });
+    document.getElementById("editTitleImage").alt="picture is uploaded";
   };
   const handleInput = (e) => {
     e.persist();
@@ -99,217 +100,114 @@ function EditTitle(props) {
     return <h4>Loading title data...</h4>;
   }
   return (
-    <div className="container-fluid px-4">
-    <div className="card mt-4">
-      <div className="card-header">
-        <h1 className="mt-4">Edit Title</h1>
-        <form
-          encType="multipart/form-data"
-          onSubmit={updateTitle}
-          id="title_form"
-        >
-          <ul className="nav nav-tabs" id="myTab" role="tablist">
-            <li className="nav-item" role="presentation">
-              <button
-                className="nav-link active"
-                id="home-tab"
-                data-bs-toggle="tab"
-                data-bs-target="#home"
-                type="button"
-                role="tab"
-                aria-controls="home"
-                aria-selected="true"
-              >
-                Home
-              </button>
-            </li>
-            <li className="nav-item" role="presentation">
-              <button
-                className="nav-link"
-                id="details-tab"
-                data-bs-toggle="tab"
-                data-bs-target="#details"
-                type="button"
-                role="tab"
-                aria-controls="details"
-                aria-selected="false"
-              >
-                Details
-              </button>
-            </li>
-          </ul>
-
-          <div className="tab-content" id="myTabContent">
-            <div
-              className="tab-pane fade show active"
-              id="home"
-              role="tabpanel"
-              aria-labelledby="home-tab"
-            >
-              {/* Home Tab Fields */}
-              <div className="form-group mb-3">
-                <label>Select Genre</label>
-                <select
-                  name="genre_id"
-                  onChange={handleInput}
-                  value={titleInput.genre_id}
-                  className="form-control"
-                >
-                  <option>-- Select Genre --</option>
-                  {genrelist.map((item) => {
-                    return (
-                      <option value={item.id} key={item.id}>
-                        {item.name}
-                      </option>
-                    );
-                  })}
-                </select>
-                <span className="text-danger">{error.genre_id}</span>
-              </div>
-              <div className="form-group mb-3">
-                <label>Slug</label>
-                <input
-                  type="text"
-                  name="slug"
-                  onChange={handleInput}
-                  value={titleInput.slug}
-                  className="form-control"
-                />
-                <span className="text-danger">{error.slug}</span>
-              </div>
-              <div className="form-group mb-3">
-                <label>Name</label>
-                <input
-                  type="text"
-                  name="name"
-                  onChange={handleInput}
-                  value={titleInput.name}
-                  className="form-control"
-                />
-                <span className="text-danger">{error.name}</span>
-              </div>
-              <div className="form-group mb-3">
-                <label>Description</label>
-                <textarea
-                  name="description"
-                  onChange={handleInput}
-                  value={titleInput.description}
-                  className="form-control"
-                />
-              </div>
-            </div>
-
-            <div
-              className="tab-pane card-body border fade"
-              id="details"
-              role="tabpanel"
-              aria-labelledby="details-tab"
-            >
-              <div className="row">
-                {/* Details Tab Fields */}
-
-                <div className="col-md-4 form-group mb-3">
-                  <label>Author</label>
-                  <input
-                    type="text"
-                    name="author"
-                    onChange={handleInput}
-                    value={titleInput.author}
-                    className="form-control"
-                  />
+    <div id="editTitlePage">
+    <div className="editTitleCard">
+      <div className="editTitleHeader">
+        <h1 className="editTitleHeaderTitle">Edit Title</h1>
+      </div>
+      <form onSubmit={updateTitle} id="editTitleForm">
+          <div id='editTitleContent' >
+            {/* Home Tab Fields */}
+            <div className='editTitleColumn'>
+                <div className="editTitleField">
+                  <div>
+                    <label>Select Genre</label>
+                    <select name="genre_id" onChange={handleInput} value={titleInput.genre_id} >
+                      <option>-- Select Genre --</option>
+                      {genrelist.map((item) => {
+                        return (
+                          <option value={item.id} key={item.id}>
+                            {item.name}
+                          </option>);})}
+                    </select>
+                  </div>
+                  <span className="text-danger">{error.genre_id}</span>
+                </div>
+                <div className="editTitleField">
+                  <div>
+                    <label>Slug</label>
+                    <input type="text" name="slug" onChange={handleInput} value={titleInput.slug}/>
+                  </div>
+                  <span className="text-danger">{error.slug}</span>
+                </div>
+                <div className="editTitleField">
+                  <div>
+                    <label>Name</label>
+                    <input type="text" name="name" onChange={handleInput} value={titleInput.name}/>
+                  </div>
+                  <span className="text-danger">{error.name}</span>
+                </div>
+                <div className="editTitleField" id='editTitleDescription'>
+                  <div>
+                  <label>Description</label>
+                  <textarea name="description" onChange={handleInput} value={titleInput.description} />
+                  </div>
+                </div>
+              {/* Details Tab Fields */}
+                <div className="editTitleField">
+                  <div>
+                    <label>Author</label>
+                    <input type="text" name="author" onChange={handleInput} value={titleInput.author}/>
+                  </div>
                   <span className="text-danger">{error.author}</span>
                 </div>
-                <div className="col-md-4 form-group mb-3">
-                  <label>Price</label>
-                  <input
-                    type="text"
-                    name="price"
-                    onChange={handleInput}
-                    value={titleInput.price}
-                    className="form-control"
-                  />
+                <div className="editTitleField">
+                  <div>
+                    <label>Price</label>
+                    <input type="text" name="price" onChange={handleInput} value={titleInput.price}/>
+                  </div>
                   <span className="text-danger">{error.price}</span>
                 </div>
-                <div className="col-md-4 form-group mb-3">
-                  <label>Rating</label>
-                  <input
-                    type="text"
-                    name="rating"
-                    onChange={handleInput}
-                    value={titleInput.rating}
-                    className="form-control"
-                  />
+                <div className="editTitleField">
+                  <div>
+                    <label>Rating</label>
+                    <input type="text"  name="rating" onChange={handleInput} value={titleInput.rating}/>
+                  </div>
                   <span className="text-danger">{error.rating}</span>
                 </div>
-                <div className="col-md-4 form-group mb-3">
-                  <label>Publisher</label>
-                  <input
-                    type="text"
-                    name="publisher"
-                    onChange={handleInput}
-                    value={titleInput.publisher}
-                    className="form-control"
-                  />
+                <div className="editTitleField">
+                  <div>
+                    <label>Publisher</label>
+                    <input type="text" name="publisher" onChange={handleInput} value={titleInput.publisher} />
+                  </div>
                   <span className="text-danger">{error.publisher}</span>
                 </div>
-                <div className="col-md-8 form-group mb-3">
-                  <label>Image</label>
-                  <input
-                    type="file"
-                    name="image"
-                    onChange={handleImage}
-                    className="form-control"
-                  />
-                  <img
-                    src={`http://localhost:8000/${titleInput.image}`}
-                    alt=""
-                    width="50px"
-                  />
-                </div>
-                <div className="col-md-4 form-group mb-3">
-                  <label>Featured</label>
-                  <input
-                    type="checkbox"
-                    name="featured"
-                    onChange={handleCheckbox}
-                    defaultChecked={allcheckbox.featured === 1 ? true : false}
-                    className="w-50 h-50"
-                  />
-                </div>
-                <div className="col-md-4 form-group mb-3">
-                  <label>Popular</label>
-                  <input
-                    type="checkbox"
-                    name="popular"
-                    onChange={handleCheckbox}
-                    defaultChecked={allcheckbox.popular === 1 ? true : false}
-                    className="w-50 h-50"
-                  />
-                </div>
-                <div className="col-md-4 form-group mb-3">
-                  <label>Status</label>
-                  <input
-                    type="checkbox"
-                    name="status"
-                    onChange={handleCheckbox}
-                    defaultChecked={allcheckbox.status === 1 ? true : false}
-                    className="w-50 h-50"
-                  />
-                </div>
               </div>
-            </div>
+             <div className='editTitleColumn'>
+               <div id='editTitleImg'>
+               <img id="editTitleImage" src={`http://localhost:8000/${titleInput.image}`} alt=""/>
+               </div>
+              <div className="editTitleField">
+                  <div id='editTitleImageField'>
+                    <label>Image</label>
+                    <input type="file" name="image" onChange={handleImage}/>
+                  </div>
+                </div>
+                <div className="editTitleField1">
+                  <div>
+                    <label>Featured</label>
+                    <input type="checkbox" name="featured" onChange={handleCheckbox} defaultChecked={allcheckbox.featured === 1 ? true : false} />
+                  </div>
+                </div>
+                <div className="editTitleField1">
+                 <div>
+                  <label>Popular</label>
+                    <input type="checkbox" name="popular" onChange={handleCheckbox} defaultChecked={allcheckbox.popular === 1 ? true : false}  />
+                 </div>
+                </div>
+                <div className="editTitleField1">
+                  <div>
+                    <label>Status</label>
+                    <input type="checkbox" name="status" onChange={handleCheckbox} defaultChecked={allcheckbox.status === 1 ? true : false}  />
+                  </div>
+                </div>
           </div>
-          <Link
-            to="/admin/show-title"
-            className="btn btn-primary mb-3 px-4 float-start"
-          >
-            Back
-          </Link>
-          <button type="submit" className="btn btn-primary px-4 float-end">
-            Update
-          </button>
-        </form>
-      </div>
+        </div>
+        <div id='editTitleBtns'>
+          <Link to="/admin/show-title"> Back</Link>
+          <button type="submit">Update </button>
+        </div>
+      </form>
     </div>
   </div>
   );
